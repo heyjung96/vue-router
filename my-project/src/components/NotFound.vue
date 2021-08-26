54d65e43f1c27cc72bb0264d8ccb9846e3c03f79
혜정 <86948318+heyjung96@users.noreply.github.com>
2021-08-26 17:01:38 +0900

    Not Found Page 추가

---
+++ my-project/src/components/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img
+      src="https://programmers.co.kr/assets/not-found-error-87f39b8807b9fc0e491814a4a6b563a294a77f5ddcc44a2db35a83076b45792c.png"
+    />
+    <h1>여긴 당신이 찾는 웹페이지가 아니에요</h1>
+    <h3>404 Page not found</h3>
+    <router-link to="/">
+      <h3>GO HOME >>></h3>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ my-project/src/components/NotFoundChildren.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src="https://t1.daumcdn.net/cfile/tistory/243F0E33585A2C622D" />
+    <router-link to="/">
+      NotFoundChildren 의 영역입니다
+      <h3>GO HOME >>></h3>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
